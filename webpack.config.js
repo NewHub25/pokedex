863838ec5dfed9a5ca92878b66afab762aa7897c
Terry Ildefonso <master_correo144@hotmail.com>
2022-09-28 10:12:55 -0500

    Se crea el lugar de trabajo

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        home: './src/home.js',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
